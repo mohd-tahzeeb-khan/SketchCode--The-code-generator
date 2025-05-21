21d07b7194b2591bcec97c4132e3996a105c7b00
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-05-21 15:39:57 +0530

    Functionality of webhook for creating new user and deleting the new user is working and ready to deploy

--- sketchcode/src/app/api/user/usercreated/route.js
+++ sketchcode/src/app/api/user/usercreated/route.js
@@ -1,16 +1,15 @@
 import { db } from "@/db/index";
 import { userTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
 export async function POST(requestdata) {
     try {
         const rawBodydata=await requestdata.text()
         const payload =JSON.parse(rawBodydata);
-        if(payload.type === "user.created"
-        ){
+        if(payload.type === "user.created")
+            {
             const {username, first_name, email_addresses, id}=payload.data;
             const clerkid=id.replace("user_", "")
             const email = email_addresses?.[0]?.email_address;
-
-            console.log(email, clerkid)
             const result=await db.insert(userTable).values({username:username || first_name,
                 emailid:email,
                 clerkid:clerkid,
@@ -20,11 +19,21 @@ export async function POST(requestdata) {
             return new Response("New User Created",{status:200
             })
         }
+        if(payload.type==="user.deleted"){
+            const{deleted, id}=payload.data;
+            const clerkid=id.replace("user_", "")
+            const result=await db.delete(userTable).where(eq(clerkid, clerkid));
+            if(deleted && result.rowCount>0){
+                return new Response("user deleted(Removed)", {status:200})
+            }
+            else{
+                return new Response("User not Exist", {status:204})
+            }
+        }
     } catch (error) {
         if(error.code == 23505){
             return new Response("User Already exist" ,{status:409})
         }
         
     }
-    
 }
