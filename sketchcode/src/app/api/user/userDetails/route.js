6f1451fc992a48bbef67a46ea161ce9bc8fc135e
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-05-23 20:24:25 +0530

    Developed and Integrated an API for Fetching details of the user from database

---
+++ sketchcode/src/app/api/user/userDetails/route.js
@@ -0,0 +1,24 @@
+import { db } from '@/db/index';
+import { userTable } from '@/db/schema';
+import { eq } from 'drizzle-orm';
+import { NextResponse } from 'next/server';
+
+export async function POST(req) {
+  const body = await req.json();
+  console.log(body)
+  const userId = body.userId;
+// return NextResponse.json(userId)
+  if (!userId) {
+    return NextResponse.json({ error: 'User ID missing' }, { status: 400 });
+  }
+  try {
+    const userData = await db
+      .select()
+      .from(userTable)
+      .where(eq(userTable.clerkid, userId));
+    return NextResponse.json(userData[0] || {});
+  } catch (error) {
+    console.error('DB Error:', error);
+    return NextResponse.json({ error: 'Failed to fetch user data' }, { status: 500 });
+  }
+}
