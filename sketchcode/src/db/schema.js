082a473550b848b04a92c9f5f3dab812449d0007
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-05-25 23:21:09 +0530

    altered the Database Schema for better Database Functionality

--- sketchcode/src/db/schema.js
+++ sketchcode/src/db/schema.js
@@ -1,4 +1,5 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { url } from "inspector";
 
 export const userTable=pgTable("users",{
     id:integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -6,4 +7,16 @@ export const userTable=pgTable("users",{
     username:varchar({length:255}).notNull(),
     emailid:varchar({length:255}).notNull().unique(),
     credits:integer().default(0)
+})
+
+
+export const ImageDetails=pgTable("Images", {
+    id:integer().primaryKey().generatedAlwaysAsIdentity(),
+    img_url:varchar({length:255}).notNull(),
+    model:varchar({length:255}).notNull(),
+    description:varchar({length:255}).notNull(),
+    code:varchar({length:255}),
+    createdby:varchar({length:255}).notNull(),
+    user_id:integer().notNull(),
+    clerk_id:varchar({length:255}).notNull()
 })
