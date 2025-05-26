afdf62c3e9853537ad40865d04b500d1178a1766
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-05-26 20:10:56 +0530

    Integrated new Features in User Authenticaiton

--- sketchcode/src/app/components/userAuth.js
+++ sketchcode/src/app/components/userAuth.js
@@ -1,25 +1,33 @@
 import { useUser } from "@clerk/nextjs";
 import { useContext } from "react";
 import { userContext } from "@/context/userContext";
-export default function UserAuth(){
-   
-    const { isSignedIn } = useUser();
-    if(!isSignedIn) return <p>Please Sign In first</p>
-    const {user, setUser}=useContext(userContext);
 
+export default function UserAuth() {
+  const { isSignedIn } = useUser();
+  const { user, setUser } = useContext(userContext); // ✅ always called
 
- return(<>
- <main>
-    <div>
-        <h1 className="uppercase">Welcome, <span className="font-bold text-2xl">{user.username}</span></h1>
-    </div>
- </main>
- <section className="shadow-2xl bg-zinc-200">
-    <div className="flex gap-7"><h2>Name:</h2><h2>{user.username}</h2></div>
-    <div className="flex gap-7"><h2>Registered Email Id:</h2><h2>{user.emailid}</h2></div>
-    <div className="flex gap-7"><h2>Cid:</h2><h2>{user.clerkId}</h2></div>
- </section>
- 
- </>)
+  if (!isSignedIn) return <p>Please Sign In first</p>;
 
+  return (
+    <>
+      <main>
+        <div>
+          <h1 className="uppercase">
+            Welcome, <span className="font-bold text-2xl">{user.username}</span>
+          </h1>
+        </div>
+      </main>
+      <section className="shadow-2xl bg-zinc-200">
+        <div className="flex gap-7">
+          <h2>Name:</h2><h2>{user.username}</h2>
+        </div>
+        <div className="flex gap-7">
+          <h2>Registered Email Id:</h2><h2>{user.emailid}</h2>
+        </div>
+        <div className="flex gap-7">
+          <h2>Cid:</h2><h2>{user.clerkId}</h2>
+        </div>
+      </section>
+    </>
+  );
 }
